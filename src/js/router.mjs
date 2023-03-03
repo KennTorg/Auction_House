4e16440ac4a14d7871cb4eb051cc1f41919de7ff
Kenn Torg <kennettorgersenmail@gmail.com>
2023-03-03 16:28:35 +0100

    Router

---
+++ src/js/router.mjs
@@ -0,0 +1,19 @@
+/*import * as listeners from "./handlers/index.mjs";
+
+export default function router() {
+  const path = location.pathname;
+
+  switch (path) {
+    case "/pages/login/":
+      listeners.setLoginFormListener();
+      listeners.setLogoutListener();
+      break;
+    case "/pages/register/":
+      listeners.setRegisterFormListener();
+      listeners.setLogoutListener();
+      break;
+    case "/index.html":
+      listeners.setLogoutListener();
+  }
+}
+*/
